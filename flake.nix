e4a9c0daa26e5248f1768279b44f12b1ab5e2357
Guilhem Saurel <guilhem.saurel@laas.fr>
2025-10-03 11:06:10 +0200

    Nix: eigen 3.4.1 + 5.0.0

--- flake.nix
+++ flake.nix
@@ -19,19 +19,27 @@
           };
           devShells.default = pkgs.mkShell { inputsFrom = [ self'.packages.default ]; };
           packages = {
-            default = self'.packages.eigenpy;
-            # Test eigen v5
-            eigen = pkgs.eigen.overrideAttrs (super: {
+            default = self'.packages.eigenpy-eigen5;
+            eigen3 = pkgs.eigen.overrideAttrs (super: rec {
+              version = "3.4.1";
               src = pkgs.fetchFromGitLab {
                 inherit (super.src) owner repo;
-                tag = "5.0.0";
-                hash = "sha256-L1KUFZsaibC/FD6abTXrT3pvaFhbYnw+GaWsxM2gaxM=";
+                tag = version;
+                hash = "sha256-NSq1tUfy2thz5gtsyASsKeYE4vMf71aSG4uXfrX86rk=";
               };
               patches = [ ];
               postPatch = "";
             });
-            eigenpy =
-              (pkgs.python3Packages.eigenpy.override { inherit (self'.packages) eigen; }).overrideAttrs
+            eigen5 = self'.packages.eigen3.overrideAttrs (super: rec {
+              version = "5.0.0";
+              src = pkgs.fetchFromGitLab {
+                inherit (super.src) owner repo;
+                tag = version;
+                hash = "sha256-L1KUFZsaibC/FD6abTXrT3pvaFhbYnw+GaWsxM2gaxM=";
+              };
+            });
+            eigenpy-eigen3 =
+              (pkgs.python3Packages.eigenpy.override { eigen = self'.packages.eigen3; }).overrideAttrs
                 (_: {
                   src = pkgs.lib.fileset.toSource {
                     root = ./.;
@@ -46,6 +54,7 @@
                     ];
                   };
                 });
+            eigenpy-eigen5 = self'.packages.eigenpy-eigen3.override { eigen = self'.packages.eigen5; };
           };
         };
     };
