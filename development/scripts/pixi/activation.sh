990250e6edf74f922bbfca4574584e5547f4884b
Joris Vaillant <joris.vaillant@inria.fr>
2025-10-23 13:59:56 +0200

    pixi: Add some ccache configuration

--- development/scripts/pixi/activation.sh
+++ development/scripts/pixi/activation.sh
@@ -20,6 +20,9 @@ then
   # On GNU/Linux, I don't know if these flags are mandatory with g++ but
   # it allow to use clang++ as compiler
   export LDFLAGS="-Wl,-rpath,$CONDA_PREFIX/lib -Wl,-rpath-link,$CONDA_PREFIX/lib -L$CONDA_PREFIX/lib"
+  # Conda compiler is named x86_64-conda-linux-gnu-c++, ccache can't resolve it
+  # (https://ccache.dev/manual/latest.html#config_compiler_type)
+  export CCACHE_COMPILERTYPE=gcc
 fi
 # Without -isystem, some LSP can't find headers
 export EIGENPY_CXX_FLAGS="$CXXFLAGS -isystem $CONDA_PREFIX/include"
